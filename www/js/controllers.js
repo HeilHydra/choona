a149ca8a1fabfc2d5eb68c98acefb04b90838429
Jay Vagharia <jayvagharia@eduroam-student-207-75.lut.ac.uk>
2015-04-15 23:23:31 +0100

    Add modal for now playing
    - Add template for now playing.
    - Add controller to open now playing modal window.
    - Add new scss file for now playing and include in global
    - Tweak playlist template and playlists   scss files to accept now
    playing changes.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -6,6 +6,25 @@ angular.module('starter.controllers', [])
 
 .controller('playlistCtrl', function($scope, $ionicModal, $timeout) {
 
+$ionicModal.fromTemplateUrl('templates/nowplaying.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.modal = modal
+  });
+
+$scope.openModal = function() {
+	$scope.modal.show()
+}
+
+$scope.closeModal = function() {
+	$scope.modal.hide();
+};
+
+$scope.$on('$destroy', function() {
+	$scope.modal.remove();
+});
+
 $scope.doRefresh = function(){
 	$scope.$broadcast('scroll.refreshComplete');
 	$scope.$apply()
