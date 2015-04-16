931195dc7b2d2aa9c99acbeccfe7bb354bd6b65e
Oliver Woodings <oliver.woodings@qubitdigital.com>
2015-04-16 11:18:32 +0100

    Cleanup

--- www/js/app.js
+++ www/js/app.js
@@ -1,90 +1,84 @@
-// Ionic Starter App
+angular.module('choona', ['ionic', 'choona.controllers'])
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic', 'starter.controllers'])
+  .run(function($ionicPlatform) {
+    $ionicPlatform.ready(function() {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      if (window.cordova && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      }
+      if (window.StatusBar) {
+        StatusBar.styleDefault();
+      }
+    });
+  })
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
-
-.config(function($stateProvider, $urlRouterProvider){
-  $stateProvider
-
-
+  .config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider
       .state('intro', {
-        url:  "/",
+        url: "/",
         templateUrl: 'templates/intro.html',
         controller: 'introCtrl'
-    })
+      })
 
-    .state('login', {
-      url: "/login",
-      templateUrl: "templates/login.html",
-      controller: 'loginCtrl'
-    })
+      .state('login', {
+        url: "/login",
+        templateUrl: "templates/login.html",
+        controller: 'loginCtrl'
+      })
 
       .state('app', {
-        url:  "/app",
-        abstract:true,
+        url: "/app",
+        abstract: true,
         templateUrl: 'templates/menu.html',
         controller: 'AppCtrl'
-    })
+      })
 
-    .state('app.playlist', {
-        url:  "/playlist",
+      .state('app.playlist', {
+        url: "/playlist",
         views: {
-          'menuContent' :{
+          'menuContent': {
             templateUrl: "templates/playlist.html",
-            controller:'playlistCtrl'
+            controller: 'playlistCtrl'
           }
         }
-    })
+      })
 
-    .state('app.activity', {
-        url:  "/activity",
+      .state('app.activity', {
+        url: "/activity",
         views: {
-          'menuContent' :{
+          'menuContent': {
             templateUrl: "templates/activity.html"
           }
         }
-    })
+      })
 
-    .state('app.history', {
-        url:  "/history",
+      .state('app.history', {
+        url: "/history",
         views: {
-          'menuContent' :{
+          'menuContent': {
             templateUrl: "templates/history.html",
           }
         }
-    })
+      })
 
-    .state('app.historysongs', {
-        url:  "/history/songs",
+      .state('app.historysongs', {
+        url: "/history/songs",
         views: {
-          'menuContent' :{
+          'menuContent': {
             templateUrl: "templates/history-detail.html",
           }
         }
-    })
+      })
 
-    .state('app.settings', {
-        url:  "/settings",
+      .state('app.settings', {
+        url: "/settings",
         views: {
-          'menuContent' :{
+          'menuContent': {
             templateUrl: "templates/settings.html"
           }
         }
-    });
+      });
+
     $urlRouterProvider.otherwise('/');
-})
+  });
